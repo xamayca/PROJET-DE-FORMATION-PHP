97f19c30552fe4aba5e4d433fcc015fe1e9e9f55
xamayca <xamalekoum@gmail.com>
2024-02-12 16:56:24 +0100

    add profile user, prepare edition

--- views/pages/account/profile.php
+++ views/pages/account/profile.php
@@ -1,92 +1,112 @@
-    <div id="user-profile">
+<div id="user-profile">
 
-        <div class="profile-header">
+    <div class="profile-header">
 
-            <!-- AFFICHE LE RÔLE DE L'UTILISATEUR -->
-            <span>
-                <?php echo isset($userData['role_name']) ?
-                    htmlspecialchars($userData['role_name']) :
-                    'Rôle non défini'; ?>
-            </span>
-
-            <img class="avatar" src="<?php echo isset($userData['avatar']) ?
-                htmlspecialchars($userData['avatar']) :
-                'assets/img/avatar-default.png'; ?>"
-                 alt="Avatar de l'utilisateur">
-
-                <h1>
-                    <?php echo isset($userData['username']) ?
-                     htmlspecialchars($userData['username']) :
-                        'Utilisateur'; ?>
-                </h1>
-
-                <?php if (isset($userData['tribe'])) : ?>
-
-                    <span>Tribu <?php echo htmlspecialchars($userData['tribe']); ?></span>
-                <?php else : ?>
-                    <span>Pas encore de tribu</span>
-                <?php endif; ?>
-        </div>
+        <button class="profile-button" type="submit">
+            <i class="fa fa-xl fa-edit"></i>
+        </button>
 
 
+        <img class="avatar" src="<?= isset($userData['avatar']) ? htmlspecialchars($userData['avatar']) : 'assets/img/avatar-default.png'; ?>" alt="Avatar de l'utilisateur">
 
+        <h1 class="<?= isset($userData['username']) ? '' : 'default-opacity'; ?>">
+            <?= isset($userData['username']) ? htmlspecialchars($userData['username']) : 'Utilisateur'; ?>
+        </h1>
 
-        <div class="profile-section">
-            <?php echo isset($userData['description']) ?
-                htmlspecialchars($userData['description']) :
-                'Vous n\'avez pas encore de description';
-            ?>
-        </div>
-
-        <div class="profile-section">
-            <ul>
-                <h2>Informations du compte</h2>
-                <?php if (isset($userData['username']) && isset($userData['role_name'])) : ?>
-                    <li>
-                        <p>Nom d'utilisateur: <?php echo htmlspecialchars($userData['username']); ?></p>
-                    </li>
-                    <li>
-                        <p>Role: <?php echo ucfirst(htmlspecialchars($userData['role_name'])); ?></p>
-                    </li>
-                <?php else : ?>
-                    <li>
-                        <p>Aucune information de compte disponible.</p>
-                    </li>
-                <?php endif; ?>
-            </ul>
-        </div>
-
-            <div class="profile-section">
-
-                <p>Nom d'utilisateur:<?php echo isset($userData['username']) ? htmlspecialchars($userData['username']) : 'Vous n\'avez pas encore de nom d\'utilisateur'; ?></p>
-                <p>Email:<?php echo isset($userData['email']) ? htmlspecialchars($userData['email']) : 'Vous n\'avez pas encore d\'email'; ?></p>
-                <?php if (isset($userData['birthdate_fr'])) : ?>
-                    <p>Date de Naissance: <?php echo htmlspecialchars($userData['birthdate_fr']); ?></p>
-                <?php endif; ?>
-                <p>Inscrit depuis: <?php echo isset($userData['registerDate_fr']) ? htmlspecialchars($userData['registerDate_fr']) : 'Non défini'; ?></p>
-                <h2>Autres Informations</h2>
-                <p>Téléphone: <?php echo isset($userData['phone']) ? htmlspecialchars($userData['phone']) : 'Non défini'; ?></p>
-            </div>
-
-        <div class="profile-section">
-                <?php if (isset($userData['signature'])) : ?>
-                   <span>" <?php echo htmlspecialchars($userData['signature']); ?> "</span>
-                <?php else : ?>
-                    <span>" Vous n'avez pas encore de signature "</span>
-               <?php endif; ?>
-        </div>
-
-            <input class="profil-button" type="submit" value="Modifier mon profil">
-
-                <?php echo isset($userData['role_name']) ?
-                    ucfirst(htmlspecialchars($userData['role_name'])) :
-                    'Non défini'; ?>
-
-                <?php echo isset($userData['registerDate_fr']) ?
-                    'Inscrit depuis ' . htmlspecialchars($userData['registerDate_fr']) :
-                    'Non défini'; ?>
-
+        <span class="tribe <?= isset($userData['tribe']) ? '' : 'default-opacity'; ?>">
+            <?= isset($userData['tribe']) ? htmlspecialchars($userData['tribe']) : 'Tribu non définie'; ?>
+        </span>
 
     </div>
 
+    <div class="desc <?= isset($userData['description']) ? '' : 'default-opacity'; ?>">
+        <?= isset($userData['description']) ? htmlspecialchars($userData['description']) : 'Description non définie'; ?>
+    </div>
 
+    <ul>
+        <li>
+            <i class="fa fa-user"></i>
+            <span class="<?= isset($userData['username']) ? '' : 'default-opacity'; ?>">
+                <?= isset($userData['username']) ? htmlspecialchars($userData['username']) : 'Username non défini'; ?>
+            </span>
+        </li>
+        <li>
+            <i class="fa fa-envelope"></i>
+            <span class="<?= isset($userData['email']) ? '' : 'default-opacity'; ?>">
+                <?= isset($userData['email']) ? htmlspecialchars($userData['email']) : 'Email non défini'; ?>
+            </span>
+        </li>
+        <li>
+            <i class="fa fa-calendar"></i>
+            <span class="<?= isset($userData['birthdate_fr']) ? '' : 'default-opacity'; ?>">
+                <?= isset($userData['birthdate_fr']) ? ucfirst(htmlspecialchars($userData['birthdate_fr'])) : 'Date de naissance non définie'; ?>
+            </span>
+        </li>
+
+        <li>
+            <i class="fa fa-phone"></i>
+            <span class="<?= !empty($userData['phone']) ? '' : 'default-opacity'; ?>">
+                <?= !empty($userData['phone']) ? htmlspecialchars($userData['phone']) : 'Numéro non défini'; ?>
+            </span>
+        </li>
+        <li>
+            <i class="fa fa-pencil"></i>
+            <span class="<?= isset($userData['signature']) ? '' : 'default-opacity'; ?>">
+                <?= isset($userData['signature']) ? htmlspecialchars($userData['signature']) : 'Signature non définie'; ?>
+            </span>
+        </li>
+    </ul>
+
+    <div class="profile-footer">
+        <span class="<?= isset($userData['registerDate_fr']) && isset($userData['role_name']) ? '' : 'default-opacity'; ?>">
+            <?= isset($userData['registerDate_fr']) && isset($userData['role_name']) ? ucfirst(htmlspecialchars($userData['role_name'])) . ' inscrit depuis le ' . htmlspecialchars($userData['registerDate_fr']) : 'Rôle ou date d\'inscription non définis'; ?>
+        </span>
+    </div>
+
+    <form id="edit-profile-form" action="/profile" method="post" class="form-group">
+        <h1>Modification du profil</h1>
+        <div class="form-group">
+            <label for="username">Pseudo d'utilisateur:</label>
+            <div class="input-with-icon">
+                <input type="text" id="username" name="username" value="<?= isset($userData['username']) ? htmlspecialchars($userData['username']) : ''; ?>">
+                <i class="fas fa-user"></i>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="tribe">Tribu:</label>
+            <div class="input-with-icon">
+                <input type="text" id="tribe" name="tribe" value="<?= isset($userData['tribe']) ? htmlspecialchars($userData['tribe']) : ''; ?>">
+                <i class="fas fa-users"></i>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description:</label>
+            <div class="input-with-icon">
+                <textarea id="description" name="description"><?= isset($userData['description']) ? htmlspecialchars($userData['description']) : ''; ?></textarea>
+                <i class="fas fa-align-left"></i>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="phone">Numéro de téléphone:</label>
+            <div class="input-with-icon">
+                <input type="text" id="phone" name="phone" value="<?= isset($userData['phone']) ? htmlspecialchars($userData['phone']) : ''; ?>">
+                <i class="fas fa-phone"></i>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="signature">Signature:</label>
+            <div class="input-with-icon">
+                <input type="text" id="signature" name="signature" value="<?= isset($userData['signature']) ? htmlspecialchars($userData['signature']) : ''; ?>">
+                <i class="fas fa-signature"></i>
+            </div>
+        </div>
+
+        <button type="submit" class="form-button">Enregistrer</button>
+    </form>
+
+
+</div>
